6e85331cbe1aab35add666a94173e7755413ac68
malf75 <malfbf6@gmail.com>
2024-06-08 10:01:38 -0300

    fix

--- src/components/Carrossel/index.js
+++ src/components/Carrossel/index.js
@@ -29,7 +29,7 @@ export default function Carrossel(){
             <div className='card'>
             <img className='imagem__card' src='./images/karen.jpg' alt='Foto da Karen'/>
             <div className='descricao'>
-            <h1 className='nome'>Karen Marianne dos Santos Lima</h1>
+            <h1 className='nome'>Roberto Matheus dos Santos Lima Tavares</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at efficitur eros. Nam pharetra felis vitae urna blandit venenatis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis laoreet mauris vel pulvinar aliquet. Fusce aliquam porta odio, vel imperdiet urna vehicula non. Nullam mattis libero justo, ac fringilla ex elementum eu. Nullam fringilla dui quis ornare rutrum. Vestibulum lacinia condimentum arcu non placerat. Aenean vehicula finibus nisi. Duis nunc ligula, blandit eget leo quis, eleifend ornare lorem. Nulla placerat semper risus, quis placerat felis suscipit at. Donec sit amet venenatis ipsum, nec hendrerit justo. Nam in maximus dui. Curabitur sollicitudin metus quis lacus tempus dapibus. Etiam sit amet augue dictum, faucibus neque ut, commodo libero. Vestibulum fermentum leo non orci tincidunt interdum sed ut nunc.</p>
             </div>
             </div>
