552942b7c15ed1225bfdf3bf773ac172731fa74d
malf75 <malfbf6@gmail.com>
2024-06-08 02:58:55 -0300

    test

--- src/components/Anchor/index.js
+++ src/components/Anchor/index.js
@@ -1,6 +1,7 @@
 import styles from './Anchor.module.css'
 
 export default function Anchor({reference, children}){
+    console.log('test')
     return(
         <a className={styles.redirect__btn} href={reference}>{children}</a>
     )
